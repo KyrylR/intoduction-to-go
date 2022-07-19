5aa9822d11ac2fe24654af1f547142026f561789
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-07-19 17:18:22 +0300

    Finish if-else (lecture)

--- lectures/demo/if-else/if-else.go
+++ lectures/demo/if-else/if-else.go
@@ -8,5 +8,18 @@ func average(a, b, c int) float32 {
 }
 
 func main() {
+	quiz1, quiz2, quiz3 := 9, 7, 8
+	if quiz1 > quiz2 {
+		fmt.Println("quiz1 > quiz2")
+	} else if quiz1 < quiz2 {
+		fmt.Println("quiz1 < quiz2")
+	} else {
+		fmt.Println("quiz1 and quiz2 are equal")
+	}
 
+	if average(quiz1, quiz2, quiz3) > 7 {
+		fmt.Println("Not bad!")
+	} else {
+		fmt.Println("Try better!")
+	}
 }

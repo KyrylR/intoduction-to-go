5ce9aca949b891eb6cfdb4927ad19bde38d7c213
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-06 13:23:42 +0300

    Finish loops (exercise, demo)

--- lectures/demo/loops/loops.go
+++ lectures/demo/loops/loops.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	sum := 0
+	fmt.Println("Sum is: ", sum)
+	for i := 0; i <= 10; i++ {
+		sum += i
+		fmt.Print(sum, " ")
+	}
 
+	fmt.Println("\nDecrement. Sum is: ", sum)
+	for sum >= 10 {
+		sum -= 5
+		fmt.Print(sum, " ")
+	}
 }
--- lectures/exercise/loops/loops.go
+++ lectures/exercise/loops/loops.go
@@ -14,5 +14,25 @@ package main
 
 import "fmt"
 
+func getValue(num int) any {
+	switch {
+	case num%3 == 0 && num%5 == 0:
+		// Print "FizzBuzz" if the integer is divisible by both 3 and 5
+		return "FizzBuzz"
+	case num%3 == 0:
+		// Print "Fizz" if the integer is divisible by 3
+		return "Fizz"
+	case num%5 == 0:
+		// Print "Buzz" if the integer is divisible by 5
+		return "Buzz"
+	default:
+		// Print just a number otherwise
+		return num
+	}
+}
+
 func main() {
+	for i := 1; i <= 50; i++ {
+		fmt.Println(getValue(i))
+	}
 }

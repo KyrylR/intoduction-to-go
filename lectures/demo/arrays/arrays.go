d97dbd8f31cd0a63b282ca0c086aa7af68b58eef
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-07 12:00:27 +0300

    Finish arrays (exercise, lecture)

--- lectures/demo/arrays/arrays.go
+++ lectures/demo/arrays/arrays.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
+type Room struct {
+	name    string
+	cleaned bool
+}
 
+func checkCleanliness(rooms [4]Room) {
+	//rooms[0].cleaned = true
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i]
+		if room.cleaned {
+			fmt.Println(room.name, "is clean")
+		} else {
+			fmt.Println(room.name, "is dirty")
+		}
+	}
+}
+
+func main() {
+	rooms := [...]Room{
+		{name: "1"},
+		{name: "2"},
+		{name: "3"},
+		{name: "4"},
+	}
+	checkCleanliness(rooms)
+	//fmt.Println(rooms[0])
+	fmt.Println("Performing cleaning...")
+	rooms[2].cleaned = true
+	rooms[3].cleaned = true
+	checkCleanliness(rooms)
 }
--- lectures/exercise/arrays/arrays.go
+++ lectures/exercise/arrays/arrays.go
@@ -18,6 +18,40 @@ package main
 
 import "fmt"
 
-func main() {
+// Product include the price and the name
+type Product struct {
+	name  string
+	price int
+}
 
+func getProductsStatus(products [4]Product) (int, int) {
+	totalNumberOfItems, totalPrice := 0, 0
+	for i := 0; i < len(products); i++ {
+		item := products[i]
+		totalPrice += item.price
+
+		if item.name != "" {
+			totalNumberOfItems += 1
+		}
+	}
+	return totalNumberOfItems, totalPrice
+}
+
+func printInfo(products [4]Product) {
+	totalNumberOfItems, totalPrice := getProductsStatus(products)
+	fmt.Println("The last item on the list:", products[totalNumberOfItems-1])
+	fmt.Println("The total number of items:", totalNumberOfItems)
+	fmt.Println("The total cost of the items:", totalPrice)
+}
+
+func main() {
+	products := [4]Product{
+		{name: "1", price: 12},
+		{name: "2", price: 2},
+		{name: "3", price: 13},
+	}
+	printInfo(products)
+	fmt.Println("Adding fourth element...")
+	products[3] = Product{name: "4", price: 4}
+	printInfo(products)
 }

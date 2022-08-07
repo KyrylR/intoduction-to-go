f56e56b060f149c2a85f0f54de6d7d37d1b2bbbd
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-07 15:17:07 +0300

    Finish rcv-func (exercise, lecture)

--- lectures/exercise/rcv-func/rcv-func.go
+++ lectures/exercise/rcv-func/rcv-func.go
@@ -16,6 +16,43 @@ package main
 
 import "fmt"
 
-func main() {
+type Value uint
 
+type EnergyStat struct {
+	energy    Value
+	maxEnergy Value
+}
+
+type HealthStat struct {
+	health    Value
+	maxHealth Value
+}
+
+type Player struct {
+	name       string
+	healthStat HealthStat
+	energyStat EnergyStat
+}
+
+func (player *Player) healAll() {
+	fmt.Println("Heal all.")
+	player.healthStat.health = player.healthStat.maxHealth
+	player.energyStat.energy = player.energyStat.maxEnergy
+}
+
+func (player Player) printStats() {
+	fmt.Println("Name ->", player.name)
+	fmt.Println("Energy ->", player.energyStat.energy)
+	fmt.Println("Health ->", player.healthStat.health)
+}
+
+func main() {
+	player := Player{
+		name:       "Bob",
+		healthStat: HealthStat{10, 100},
+		energyStat: EnergyStat{10, 100},
+	}
+	player.printStats()
+	player.healAll()
+	player.printStats()
 }

b8959c1d724b7dd034d3372d80bd86973d12fb07
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-07 12:33:37 +0300

    Finish slices (exercise, lecture)

--- lectures/demo/slices/slices.go
+++ lectures/demo/slices/slices.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
+func printSlice(title string, slice []string) {
+	fmt.Println()
+	fmt.Println("---", title, "---")
+	for i := 0; i < len(slice); i++ {
+		elem := slice[i]
+		fmt.Println(elem)
+	}
+}
 
+func main() {
+	route := []string{"1", "2", "3"}
+	printSlice("Route 1", route)
+
+	route = append(route, "Home")
+	printSlice("Route 2", route)
+
+	fmt.Println()
+	fmt.Println(route[0], "visited")
+	fmt.Println(route[1], "visited")
+
+	route = route[2:]
+	printSlice("Other", route)
 }
--- lectures/exercise/slices/slices.go
+++ lectures/exercise/slices/slices.go
@@ -18,5 +18,27 @@ import "fmt"
 
 type Part string
 
+// * Create a function to print out the contents of the assembly line
+func printStatus(title string, assemblyLine []Part) {
+	fmt.Println()
+	fmt.Println("---", title, "---")
+	for i := 0; i < len(assemblyLine); i++ {
+		elem := assemblyLine[i]
+		fmt.Print(elem, " ")
+	}
+}
+
 func main() {
+	//* Using a slice, create an assembly line that contains type Part
+	//  - Create an assembly line having any three parts
+	assemblyLine := []Part{"1", "2", "3"}
+	printStatus("Step 1", assemblyLine)
+
+	//* Perform the following:
+	//  - Add two new parts to the line
+	assemblyLine = append(assemblyLine, "4", "5")
+	printStatus("Step 2", assemblyLine)
+	//  - Slice the assembly line so it contains only the two new parts
+	assemblyLine = assemblyLine[3:]
+	printStatus("Step 3", assemblyLine)
 }

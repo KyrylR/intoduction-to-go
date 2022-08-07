1509d81847c7af12598fb2c19d69a67f4aa0d223
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-07 13:47:47 +0300

    Finish pointers (exercise, lecture)

--- lectures/demo/pointers/pointers.go
+++ lectures/demo/pointers/pointers.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
+type Counter struct {
+	hits int
+}
 
+func increment(counter *Counter) {
+	counter.hits += 1
+	fmt.Println("Counter", counter)
+}
+
+func replace(old *string, new string, counter *Counter) {
+	*old = new
+	increment(counter)
+}
+
+func main() {
+	counter := Counter{}
+
+	hello := "hello"
+	world := "world"
+	fmt.Println(hello, world)
+
+	replace(&hello, "hi", &counter)
+	fmt.Println(hello, world)
+	fmt.Println("Counter in main", counter)
+
+	phrase := []string{hello, world}
+	fmt.Println(phrase)
+	replace(&phrase[1], "Go!", &counter)
+
+	fmt.Println(phrase)
+	fmt.Println("Counter in main", counter)
 }
--- lectures/exercise/pointers/pointers.go
+++ lectures/exercise/pointers/pointers.go
@@ -18,6 +18,58 @@ package main
 
 import "fmt"
 
-func main() {
+//* Create a structure to store items and their security tag state
+//  - Security tags have two states: active (true) and inactive (false)
 
+const (
+	active   = true
+	inactive = false
+)
+
+type Tag bool
+
+type Item struct {
+	name string
+	tag  Tag
+}
+
+// * Create functions to activate and deactivate security tags using pointers
+func changeTagStatusOfItem(item *Item) {
+	switch item.tag {
+	case active:
+		item.tag = inactive
+	case inactive:
+		item.tag = active
+	default:
+		panic("Unhandled security tag status")
+	}
+}
+
+// * Create a checkout() function which can deactivate all tags in a slice
+func checkout(items *[]Item) {
+	for i := 0; i < len(*items); i++ {
+		(*items)[i].tag = inactive
+	}
+	//for _, item := range *items {
+	//	item.tag = inactive
+	//}
+}
+
+func main() {
+	//* Perform the following:
+	//  - Create at least 4 items, all with active security tags
+	item1 := Item{"1", active}
+	item2 := Item{"2", active}
+	item3 := Item{"3", active}
+	item4 := Item{"4", active}
+	//  - Store them in a slice or array
+	items := []Item{item1, item2, item3, item4}
+	fmt.Println(items)
+	//  - Deactivate any one security tag in the array/slice
+	changeTagStatusOfItem(&items[2])
+	fmt.Println(items)
+	//  - Call the checkout() function to deactivate all tags
+	checkout(&items)
+	fmt.Println(items)
+	//  - Print out the array/slice after each change
 }

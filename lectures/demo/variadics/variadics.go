8e79af59aa3b81405aa94cff07b5a7f81398cab4
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-07 15:36:47 +0300

    Finish variadics (exercise, lecture)

--- lectures/demo/variadics/variadics.go
+++ lectures/demo/variadics/variadics.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
-func main() {
+func sum(nums ...int) int {
+	sum := 0
+	for _, item := range nums {
+		sum += item
+	}
+	return sum
+}
 
+func main() {
+	fmt.Println(sum(1, 2, 3, 4, 5))
+	fmt.Println(sum(1, 2))
+
+	a := []int{1, 2, 3}
+	b := []int{4, 5, 6}
+	combination := append(a, b...)
+	fmt.Println(sum(a...))
+	fmt.Println(sum(b...))
+	fmt.Println(sum(combination...))
 }

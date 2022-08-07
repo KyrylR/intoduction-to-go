5c9e7a2e078fe06992d27d055403df8f1242ce80
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-07 15:32:29 +0300

    Finish iota (exercise, lecture)

--- lectures/exercise/iota/iota.go
+++ lectures/exercise/iota/iota.go
@@ -16,12 +16,41 @@ package main
 
 import "fmt"
 
+type Operation int
+
+const (
+	Add Operation = iota
+	Subtract
+	Multiply
+	Divide
+)
+
+func (op Operation) calculate(lhs, rhs int) int {
+	switch op {
+	case Add:
+		return lhs + rhs
+	case Subtract:
+		return lhs - rhs
+	case Multiply:
+		return lhs * rhs
+	case Divide:
+		return lhs / rhs
+	default:
+		panic("Unhandled operation.")
+	}
+}
+
 func main() {
+	// In case of: Add ---(without type Operation)--- = iota, we need to use following syntax
+	add := Operation(Add)
 	fmt.Println(add.calculate(2, 2)) // = 4
 
+	sub := Subtract
 	fmt.Println(sub.calculate(10, 3)) // = 7
 
+	mul := Multiply
 	fmt.Println(mul.calculate(3, 3)) // = 9
 
+	div := Divide
 	fmt.Println(div.calculate(100, 2)) // = 50
 }

442513f1c0389497c6beee127ff020bd774f8232
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-07 12:43:16 +0300

    Finish ranges (exercise, lecture)

--- lectures/demo/ranges/ranges.go
+++ lectures/demo/ranges/ranges.go
@@ -3,4 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	slice := []string{"Hello", "world", "!"}
+
+	for i, elem := range slice {
+		fmt.Println(i, elem)
+		for _, ch := range elem {
+			fmt.Printf("%q - %v - %T ", ch, ch, ch)
+		}
+		fmt.Println()
+	}
 }

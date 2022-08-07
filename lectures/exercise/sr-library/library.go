c1ad8cff4d10ffe2b0866b37a41f39d7e6862aa4
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-07 14:21:53 +0300

    Created a program to manage lending of library books.

--- lectures/exercise/sr-library/library.go
+++ lectures/exercise/sr-library/library.go
@@ -19,8 +19,150 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Title string // book title
+type Name string  // library member name
+
+type LendAudit struct {
+	checkOut time.Time
+	checkIn  time.Time
+}
+
+type Member struct {
+	name  Name
+	books map[Title]LendAudit
+}
+
+type BookEntry struct {
+	total  int // total owned by the library
+	loaned int // total books loaned out
+}
+
+type Library struct {
+	members map[Name]Member
+	books   map[Title]BookEntry
+}
+
+func printMemberAudit(member *Member) {
+	for title, audit := range member.books {
+		var returnTime string
+		if audit.checkIn.IsZero() {
+			returnTime = "[not returned yet]"
+		} else {
+			returnTime = audit.checkIn.String()
+		}
+		fmt.Println(member.name, ":", title, ":", audit.checkOut.String(), ":", returnTime)
+	}
+}
+
+func printMember(library *Library) {
+	for _, member := range library.members {
+		printMemberAudit(&member)
+	}
+}
+
+func printLibraryBooks(library *Library) {
+	fmt.Println()
+	for title, book := range library.books {
+		fmt.Println(title, "/ total:", book.total, "/ loaned:", book.loaned)
+	}
+	fmt.Println()
+}
+
+func checkoutBook(library *Library, title Title, member *Member) bool {
+	book, found := library.books[title]
+	if !found {
+		fmt.Println("Book not party of library")
+		return false
+	}
+	if book.loaned == book.total {
+		fmt.Println("No more books available to lend")
+		return false
+	}
+
+	book.loaned += 1
+	library.books[title] = book
+
+	member.books[title] = LendAudit{checkOut: time.Now(), checkIn: time.Time{}}
+	return true
+}
+
+func returnBook(library *Library, title Title, member *Member) bool {
+	book, found := library.books[title]
+	if !found {
+		fmt.Println("Book not party of library")
+		return false
+	}
+
+	audit, found := member.books[title]
+	if !found {
+		fmt.Println("Member did not check out this book")
+		return false
+	}
+
+	if !audit.checkIn.IsZero() {
+		fmt.Println("Member has already returned a book")
+		return false
+	}
+
+	book.loaned -= 1
+	library.books[title] = book
+
+	audit.checkIn = time.Now()
+	member.books[title] = audit
+	return true
+}
 
 func main() {
+	library := Library{
+		books:   make(map[Title]BookEntry),
+		members: make(map[Name]Member),
+	}
+	//  - Add at least 4 books...
+	library.books["Webapps in Go"] = BookEntry{
+		total:  4,
+		loaned: 0,
+	}
+	library.books["The Little Go Book"] = BookEntry{
+		total:  3,
+		loaned: 0,
+	}
+	library.books["Let's learn Go"] = BookEntry{
+		total:  2,
+		loaned: 0,
+	}
+	library.books["Go Bootcamp"] = BookEntry{
+		total:  1,
+		loaned: 0,
+	}
+	//  ... and at least 3 members to the library
+	library.members["Jayson"] = Member{"Jayson", make(map[Title]LendAudit)}
+	library.members["Billy"] = Member{"Billy", make(map[Title]LendAudit)}
+	library.members["Susanna"] = Member{"Susanna", make(map[Title]LendAudit)}
 
+	fmt.Println("\nInitial:")
+	//  - Print out initial library information, and after each change
+	printLibraryBooks(&library)
+	printMember(&library)
+
+	//  - Check out a book
+	member := library.members["Jayson"]
+	checkedOut := checkoutBook(&library, "Go Bootcamp", &member)
+	fmt.Println("\nCheck out a book:")
+	if checkedOut {
+		printLibraryBooks(&library)
+		printMember(&library)
+	}
+
+	//  - Check in a book
+	returned := returnBook(&library, "Go Bootcamp", &member)
+	fmt.Println("\nCheck in a book:")
+	if returned {
+		printLibraryBooks(&library)
+		printMember(&library)
+	}
 }

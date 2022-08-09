8685db632fe6ed589766b535120eaac1623cbb2e
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-09 13:49:23 +0300

    Finish interfaces (exercise, lecture)

--- lectures/demo/interfaces/interfaces.go
+++ lectures/demo/interfaces/interfaces.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
+type Preparer interface {
+	PrepareDish()
+}
 
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("Cook chicken")
+}
+
+func (c Salad) PrepareDish() {
+	fmt.Println("Chop salad")
+}
+
+func prepareDishes(dishes []Preparer) {
+	fmt.Println("Prepare dishes:")
+	for _, dish := range dishes {
+		fmt.Printf("--Dish: %v--\n", dish)
+		dish.PrepareDish()
+	}
+	fmt.Println()
+}
+
+func main() {
+	dishes := []Preparer{Chicken("Chicken Wings"), Salad("Caesar")}
+	prepareDishes(dishes)
 }
--- lectures/exercise/interfaces/interfaces.go
+++ lectures/exercise/interfaces/interfaces.go
@@ -1,26 +1,56 @@
-//--Summary:
-//  Create a program that directs vehicles at a mechanic shop
-//  to the correct vehicle lift, based on vehicle size.
+// --Summary:
 //
-//--Requirements:
-//* The shop has lifts for multiple vehicle sizes/types:
-//  - Motorcycles: small lifts
-//  - Cars: standard lifts
-//  - Trucks: large lifts
-//* Write a single function to handle all of the vehicles
-//  that the shop works on.
-//* Vehicles have a model name in addition to the vehicle type:
-//  - Example: "Truck" is the vehicle type, "Road Devourer" is a model name
-//* Direct at least 1 of each vehicle type to the correct
-//  lift, and print out the vehicle information.
+//	Create a program that directs vehicles at a mechanic shop
+//	to the correct vehicle lift, based on vehicle size.
 //
-//--Notes:
-//* Use any names for vehicle models
-
+// --Requirements:
+// * The shop has lifts for multiple vehicle sizes/types:
+//   - Motorcycles: small lifts
+//   - Cars: standard lifts
+//   - Trucks: large lifts
+//   - Write a single function to handle all of the vehicles
+//     that the shop works on.
+//   - Vehicles have a model name in addition to the vehicle type:
+//   - Example: "Truck" is the vehicle type, "Road Devourer" is a model name
+//   - Direct at least 1 of each vehicle type to the correct
+//     lift, and print out the vehicle information.
+//
+// --Notes:
+// * Use any names for vehicle models
 package main
 
 import "fmt"
 
-func main() {
+type Lifter interface {
+	printLiftType()
+}
 
+type Motorcycle string
+type Car string
+type Truck string
+
+func (m Motorcycle) printLiftType() {
+	fmt.Println("Small lift")
+}
+
+func (m Car) printLiftType() {
+	fmt.Println("Standard lift")
+}
+
+func (m Truck) printLiftType() {
+	fmt.Println("Large lift")
+}
+
+func printAllInfo(vehicles []Lifter) {
+	fmt.Println("Show vehicles lifts:")
+	for _, vehicle := range vehicles {
+		fmt.Printf("--Vehicle: %v--\n", vehicle)
+		vehicle.printLiftType()
+	}
+	fmt.Println()
+}
+
+func main() {
+	vehicles := []Lifter{Motorcycle("A"), Car("B"), Truck("C")}
+	printAllInfo(vehicles)
 }

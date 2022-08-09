cc147e106f162b65d992527c67207e7d5dda39f2
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-09 20:53:15 +0300

    Finish goroutines (exercise, lecture)

--- lectures/exercise/goroutines/goroutines.go
+++ lectures/exercise/goroutines/goroutines.go
@@ -34,6 +34,58 @@ import (
 	"time"
 )
 
+// --Requirements:
+// * Sum the numbers in each file noted in the main() function
+func sumFile(rd bufio.Reader) int {
+	sum := 0
+	for {
+		line, err := rd.ReadString('\n')
+		if err == io.EOF {
+			return sum
+		}
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+		num, err := strconv.Atoi(line[:len(line)-1])
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+
+		sum += num
+	}
+}
+
+//* Add each sum together to get a grand total for all files
+//  - Print the grand total to the terminal
+//* Launch a goroutine for each file
+//* Report any errors to the terminal
+
 func main() {
 	files := []string{"num1.txt", "num2.txt", "num3.txt", "num4.txt", "num5.txt"}
+	sum := 0
+
+	for i := 0; i < len(files); i++ {
+		file, err := os.Open(files[i])
+		if err != nil {
+			//* Report any errors to the terminal
+			fmt.Println("Error:", err)
+			return
+		}
+
+		rd := bufio.NewReader(file)
+
+		calculate := func() {
+			fileSum := sumFile(*rd)
+			//* Add each sum together to get a grand total for all files
+			sum += fileSum
+		}
+		//* Launch a goroutine for each file
+		go calculate()
+	}
+
+	// Need to wait for goroutines to finish. Increase if needed.
+	time.Sleep(1000 * time.Millisecond)
+
+	//  - Print the grand total to the terminal
+	fmt.Println(sum)
 }

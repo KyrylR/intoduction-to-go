60a8a2212e3879bb75375d5d714ad7caeb6dfe17
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-09 13:49:58 +0300

    Finish interfaces() + better sol (exercise, lecture)

--- lectures/exercise/interfaces/interfaces.go
+++ lectures/exercise/interfaces/interfaces.go
@@ -21,36 +21,73 @@ package main
 
 import "fmt"
 
-type Lifter interface {
-	printLiftType()
+// * The shop has lifts for multiple vehicle sizes/types:
+//   - Motorcycles: small lifts
+//   - Cars: standard lifts
+//   - Trucks: large lifts
+const (
+	SmallLift = iota
+	StandardLift
+	LargeLift
+)
+
+type Lift int
+
+type LiftPicker interface {
+	PickLift() Lift
 }
 
 type Motorcycle string
 type Car string
 type Truck string
 
-func (m Motorcycle) printLiftType() {
-	fmt.Println("Small lift")
+// * Vehicles have a model name in addition to the vehicle type:
+func (m Motorcycle) String() string {
+	return fmt.Sprintf("Motorcycle: %v", string(m))
 }
 
-func (m Car) printLiftType() {
-	fmt.Println("Standard lift")
+// * Vehicles have a model name in addition to the vehicle type:
+func (c Car) String() string {
+	return fmt.Sprintf("Car: %v", string(c))
 }
 
-func (m Truck) printLiftType() {
-	fmt.Println("Large lift")
+// * Vehicles have a model name in addition to the vehicle type:
+func (t Truck) String() string {
+	return fmt.Sprintf("Truck: %v", string(t))
 }
 
-func printAllInfo(vehicles []Lifter) {
-	fmt.Println("Show vehicles lifts:")
-	for _, vehicle := range vehicles {
-		fmt.Printf("--Vehicle: %v--\n", vehicle)
-		vehicle.printLiftType()
+func (m Motorcycle) PickLift() Lift {
+	return SmallLift
+}
+
+func (c Car) PickLift() Lift {
+	return StandardLift
+}
+
+func (t Truck) PickLift() Lift {
+	return LargeLift
+}
+
+//   - Write a single function to handle all of the vehicles
+//     that the shop works on.
+func sendToLift(p LiftPicker) {
+	switch p.PickLift() {
+	case SmallLift:
+		fmt.Printf("send %v to small lift\n", p)
+	case StandardLift:
+		fmt.Printf("send %v to standard lift\n", p)
+	case LargeLift:
+		fmt.Printf("send %v to large lift\n", p)
 	}
-	fmt.Println()
 }
 
 func main() {
-	vehicles := []Lifter{Motorcycle("A"), Car("B"), Truck("C")}
-	printAllInfo(vehicles)
+	car := Car("Sporty")
+	truck := Truck("MountainCrusher")
+	motorcycle := Motorcycle("Croozer")
+	//* Direct at least 1 of each vehicle type to the correct
+	//  lift, and print out the vehicle information.
+	sendToLift(car)
+	sendToLift(truck)
+	sendToLift(motorcycle)
 }

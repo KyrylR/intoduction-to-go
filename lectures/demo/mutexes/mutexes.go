db13d0978220dbbfc1b836747f6fbd6422ba7e35
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-10 13:52:08 +0300

    Finish sync (exercise, lecture)

--- lectures/demo/mutexes/mutexes.go
+++ lectures/demo/mutexes/mutexes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -10,6 +11,38 @@ func wait() {
 	time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 }
 
+type Hits struct {
+	count int
+	sync.Mutex
+}
+
+func serve(wg *sync.WaitGroup, hits *Hits, iteration int) {
+	wait()
+	hits.Lock()
+	defer hits.Unlock()
+	defer wg.Done()
+	hits.count += 1
+	fmt.Println("Served iteration", iteration)
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
+
+	var wg sync.WaitGroup
+	hitCounter := Hits{}
+	for i := 0; i < 20000; i++ {
+		iteration := i
+		wg.Add(1)
+		go serve(&wg, &hitCounter, iteration)
+	}
+
+	fmt.Println("Waiting for goroutines to finish...")
+	wg.Wait()
+	fmt.Println("Done!")
+
+	hitCounter.Lock()
+	totalHits := hitCounter.count
+	hitCounter.Unlock()
+
+	fmt.Printf("\nTotal hits = %d", totalHits)
 }
